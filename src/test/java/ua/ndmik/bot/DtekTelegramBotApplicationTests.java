b6e04bde7ba45f05ae91b5a6d11ba891add6d904
ndmik <255708774+ndmik-dev@users.noreply.github.com>
2026-02-02 19:26:15 +0200

    fix test DS config

--- src/test/java/ua/ndmik/bot/DtekTelegramBotApplicationTests.java
+++ src/test/java/ua/ndmik/bot/DtekTelegramBotApplicationTests.java
@@ -2,8 +2,10 @@ package ua.ndmik.bot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DtekTelegramBotApplicationTests {
 
     @Test
