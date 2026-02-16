9345e981720e2c2194cc5d236693d351528d571b
ndmik <255708774+ndmik-dev@users.noreply.github.com>
2026-02-17 00:03:26 +0200

    update timezone globally

--- src/main/java/ua/ndmik/bot/DtekTelegramBotApplication.java
+++ src/main/java/ua/ndmik/bot/DtekTelegramBotApplication.java
@@ -3,10 +3,13 @@ package ua.ndmik.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DtekTelegramBotApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(DtekTelegramBotApplication.class, args);
     }
 }
